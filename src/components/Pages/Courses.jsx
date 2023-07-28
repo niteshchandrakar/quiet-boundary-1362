0112603d1d81013cc7b10f03cdf208e3be97de64
Nitesh <niteshchandrakar2@gmail.com>
2023-07-28 15:38:17 +0530

    finall

--- src/components/Pages/Courses.jsx
+++ src/components/Pages/Courses.jsx
@@ -12,18 +12,6 @@ function Courses() {
 	const [category, setCategory] = useState("")
   const[loading,setLoading]=useState(false)
   let totalpages=[]
-
-
-const Ccategory=(e)=>{
-  setCategory(e)
-  setPagenumber(1)
-  console.log(pagenumber,category)
-}
-const Ssortby=(e)=>{
-  setSortby(e)
-  setPagenumber(1)
-}
-
   let url=`http://localhost:8080/courses?_page=${pagenumber}&_limit=4`
   const fetchdata = async (e) => {
 setLoading(true)
@@ -75,11 +63,11 @@ console.log(totalpages)
 <div className={styles.maindiv}>
   
   
-  <button className="button" value="Physics" onClick={(e)=>{Ccategory(e.target.value)}}>Physics</button>
-  <button className="button" value="Maths" onClick={(e)=>{Ccategory(e.target.value)}}>Maths</button>
-  <button className="button" value="Chemistry" onClick={(e)=>{Ccategory(e.target.value)}}>Chemistry</button>
-  <button className="button" value="English" onClick={(e)=>{Ccategory(e.target.value)}}>English</button>
-  <select className="button" onChange={(e)=>{Ssortby(e.target.value)}}  >
+  <button className="button" value="Physics" onClick={(e)=>{setCategory(e.target.value)}}>Physics</button>
+  <button className="button" value="Maths" onClick={(e)=>{setCategory(e.target.value)}}>Maths</button>
+  <button className="button" value="Chemistry" onClick={(e)=>{setCategory(e.target.value)}}>Chemistry</button>
+  <button className="button" value="English" onClick={(e)=>{setCategory(e.target.value)}}>English</button>
+  <select className="button" onChange={(e)=>{setSortby(e.target.value)}}  >
          <option value="">Sort By</option>
          <option value="asc">Price Low to High</option>
          <option value="desc">Price High to Low</option>
@@ -87,7 +75,7 @@ console.log(totalpages)
 </div>
 <div className={styles.content} >
 
-      
+   
 {loading?(
   <div>
     <h1 style={{paddingBottom:"100%",color:"#4035B8"}}>Data is Loading . . .</h1>
@@ -100,13 +88,13 @@ console.log(totalpages)
       
       
       <img style={{width:"100%",height:"50%"}} src={e.image} alt="" />
-      <h5 style={{margin:"0"}}>Subject : {e.subject}</h5>
-      <p style={{margin:"0"}}>For : {e.class}</p>
-      <p style={{margin:"0"}}>Teacher: {e.Teacher}</p>
+      <h5 style={{margin:"0"}}><label style={{color:"#4035b8"}}>Subject:</label> {e.subject}</h5>
+      <p style={{margin:"0"}}><label style={{color:"#4035b8"}}>For:</label> {e.class}</p>
+      <p style={{margin:"0"}}><label style={{color:"#4035b8"}}>Teacher:</label> {e.Teacher}</p>
       
-      <p style={{margin:"0"}}>Descreption: {e.description}</p>
-      <p style={{margin:"2px"}}>starts on:{e.start}</p>
-      <p style={{margin:"2px"}}>Course Fees: Rs. {e.Fee} <button className="button" onClick={()=>{alert("Course will be added to Your Profile Shortly")}}>Buy Course</button></p>
+      <p style={{margin:"0"}}><label style={{color:"#4035b8"}}>Description:</label> {e.description}</p>
+      <p style={{margin:"2px"}}><label style={{color:"#4035b8"}}>Starts On:</label>{e.start}</p>
+      <p style={{margin:"2px"}}><label style={{color:"#4035b8"}}>Course Fee:</label> Rs. {e.Fee} <button className="button" onClick={()=>{alert("Course will be added to Your Profile Shortly")}}>Buy Course</button></p>
       
     </div>
   ))
